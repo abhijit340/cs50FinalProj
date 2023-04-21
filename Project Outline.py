7086dc73601fda09e37c25796b382a8e4ae1b52c
abhijit340 <abhijit340@gmail.com>
2023-04-21 17:57:39 +0000

    Adding project Outline

---
+++ Project Outline.py
@@ -0,0 +1,25 @@
+# Project Outline
+
+"""
+Title: Trip Itinerary Planner 
+
+Function: Help user plan a trip to a city
+
+Mininum Viable Product
+- Input Activity Name, Location, and Notes
+- Connect to Googple Maps API to drops pin at activity 
+- Allow user to set activity order
+- Map and dispaly travel times between activities
+- Let user order activies to find optimal schedule
+- Display map with icons indicating order and travel times
+    
+
+
+Extra Features 
+- Add times for activites
+- Option to optimize routing for user
+- Add suggested activites nearby
+- 
+
+
+"""
