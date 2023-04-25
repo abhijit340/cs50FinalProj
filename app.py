274bd9d72fe723a469c14cd349e2c1bc78e09112
abhijit340 <abhijit340@gmail.com>
2023-04-25 17:25:53 +0000

    bones of login fuction started

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session
+from helpers import login_req, 
 
 app = Flask(__name__)
 
 @app.route('/')
+@login_req
 def index():
     return "Hello World"
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
-
+@app.route('/register', methods=["GET", "POST"])
+def register():
+        #get path sends user to registration screen
+        if request.method == "GET":
+            return render_template("register.html")    
+        if request.method == "POST":
+             #registion action begins here
+             newUser = request.form.get("username")
+             newPass = request.form.get("password")
+             newPassCom = request.form.get("password_rep")
 
 
 if __name__ == "__main__":
